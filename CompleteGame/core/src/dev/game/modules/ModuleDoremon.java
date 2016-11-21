e3c1c47c3a03fb43b6fc7d4599d314783b1793eb
Ziggy192 <luuquangnghia97@gmail.com>
2016-11-22 00:39:26 +0700

    CompleteGame: Store highscore + Fix logic bug Kanji

--- CompleteGame/core/src/dev/game/modules/ModuleDoremon.java
+++ CompleteGame/core/src/dev/game/modules/ModuleDoremon.java
@@ -1,6 +1,7 @@
 package dev.game.modules;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -8,16 +9,22 @@ import dev.game.doremon.gfx.Assets;
 import dev.game.doremon.main.DoremonHandler;
 import dev.game.doremon.states.GameStateManager;
 import dev.game.doremon.states.MenuState;
+import dev.game.main.CompleteGame;
 import dev.game.main.GameHandler;
 
 /**
  * Created by Lam Ngo on 11/21/2016.
  */
 public class ModuleDoremon extends Module {
+    private static final java.lang.String BEST_SCORE_KEY = "Doremon_best_score";
+
     public static OrthographicCamera camera;
     public static int bestScore = 0;
     private DoremonHandler doremonHandler;
     private GameStateManager gsm;
+
+
+    private static Preferences preferences;
 
     public ModuleDoremon(GameHandler gameHandler, GameModuleManager cpanel) {
         super(gameHandler, cpanel);
@@ -26,6 +33,9 @@ public class ModuleDoremon extends Module {
     }
 
     private void init() {
+        preferences = Gdx.app.getPreferences(CompleteGame.class.getName());
+        bestScore = preferences.getInteger(BEST_SCORE_KEY, 0);
+
         camera = new OrthographicCamera();
         camera.setToOrtho(true);
 
@@ -45,6 +55,14 @@ public class ModuleDoremon extends Module {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         gsm.render(batch);
+    }
+
+    public static void updateBestScore(int score) {
+        if (score > bestScore) {
+            bestScore = score;
+            preferences.putInteger(BEST_SCORE_KEY, bestScore);
+            preferences.flush();
+        }
     }
 
     @Override
