bc1583522cb7055eb9d1d00c3f14f82f3cffb250
Ziggy192 <luuquangnghia97@gmail.com>
2016-11-22 17:46:06 +0700

    CompleteGame (minor) Kanji title alignment

--- CompleteGame/desktop/src/dev/game/desktop/DesktopLauncher.java
+++ CompleteGame/desktop/src/dev/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import dev.game.main.CompleteGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 360;
-        config.height = 640;
+        config.width = 300;
+        config.height = 500;
         new LwjglApplication(new CompleteGame(), config);
     }
 }
